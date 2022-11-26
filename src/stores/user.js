8dba46621bdcecc437d5ee12547e3c16a2a72eff
Martim Silva <martimtsilva@hotmail.com>
2022-11-26 16:35:22 +0000

    Remove components from old theme

--- src/stores/user.js
+++
@@ -1,7 +0,0 @@
-import { ref, computed, inject } from "vue";
-import { defineStore } from "pinia";
-
-export const useUserStore = defineStore("user", () => {
-  const axios = inject("axios");
-  const toast = inject("toast");
-});
