b8975d0b80fc32adab70a0472022e4ae8d584ff7
Martim Silva <martimtsilva@hotmail.com>
2022-11-28 13:01:49 +0000

    New App Splash Screen

--- src/layouts/components/DrawerContent.vue
+++ src/layouts/components/DrawerContent.vue
@@ -9,113 +9,81 @@ VerticalNavSectionTitle
 <template>
   <!-- 👉 Nav header -->
   <div class="nav-header">
-    <RouterLink
-      to="/"
-      class="app-logo d-flex align-center gap-x-3 app-title-wrapper"
-    >
-      <!-- ℹ️ You can also use img tag or VImg here -->
+    <RouterLink to="/" class="app-logo d-flex align-center gap-x-3 app-title-wrapper">
       <div v-html="logo" />
-
-      <Transition name="vertical-nav-app-title">
-        <h1 class="font-weight-semibold leading-normal text-xl text-uppercase">
-          FASTUGA
-        </h1>
-      </Transition>
     </RouterLink>
   </div>
 
   <!-- 👉 Nav items -->
   <ul>
-    <VerticalNavLink
-      :item="{
-        title: 'Dashboard',
-        to: 'index',
-        icon: { icon: 'mdi-home-outline' }
-      }"
-    />
-    <VerticalNavLink
-      :item="{
-        title: 'Account Settings',
-        to: 'account-settings',
-        icon: { icon: 'mdi-account-cog-outline' }
-      }"
-    />
-    <VerticalNavLink
-      :item="{
-        title: 'Employees',
-        to: 'users',
-        icon: { icon: 'mdi-account-group-outline' }
-      }"
-    />
-    
+    <VerticalNavLink :item="{
+      title: 'Dashboard',
+      to: 'index',
+      icon: { icon: 'mdi-home-outline' }
+    }" />
+    <VerticalNavLink :item="{
+      title: 'Account Settings',
+      to: 'account-settings',
+      icon: { icon: 'mdi-account-cog-outline' }
+    }" />
+    <VerticalNavLink :item="{
+      title: 'Employees',
+      to: 'users',
+      icon: { icon: 'mdi-account-group-outline' }
+    }" />
+
     <!-- 👉 Pages -->
     <VerticalNavSectionTitle :item="{ heading: 'Pages' }" />
-    <VerticalNavLink
-      :item="{
-        title: 'Login',
-        to: 'login',
-        target: '_blank',
-        icon: { icon: 'mdi-login' }
-      }"
-    />
-    <VerticalNavLink
-      :item="{
-        title: 'Register',
-        to: 'register',
-        target: '_blank',
-        icon: { icon: 'mdi-account-plus-outline' },
-        disable: true,
-
-      }"
-    />
+    <VerticalNavLink :item="{
+      title: 'Login',
+      to: 'login',
+      target: '_blank',
+      icon: { icon: 'mdi-login' }
+    }" />
+    <VerticalNavLink :item="{
+      title: 'Register',
+      to: 'register',
+      target: '_blank',
+      icon: { icon: 'mdi-account-plus-outline' },
+      disable: true,
+    
+    }" />
 
     <!-- ℹ️ This path doesn't exist so 404 route will catch this undefined path -->
-    <VerticalNavLink
-      :item="{
-        title: 'Error',
-        to: { path: '/error' },
-        target: '_blank',
-        icon: { icon: 'mdi-alert-circle-outline' }
-      }"
-    />
+    <VerticalNavLink :item="{
+      title: 'Error',
+      to: { path: '/error' },
+      target: '_blank',
+      icon: { icon: 'mdi-alert-circle-outline' }
+    }" />
 
     <!-- 👉 User Interface -->
     <VerticalNavSectionTitle :item="{ heading: 'User Interface' }" />
 
-    <VerticalNavLink
-      :item="{
-        title: 'Typography',
-        to: 'typography',
-        icon: { icon: 'mdi-alpha-t-box-outline' }
-      }"
-    />
-    <VerticalNavLink
-      :item="{
-        title: 'Icons',
-        to: 'icons',
-        icon: { icon: 'mdi-eye-outline' }
-      }"
-    />
-    <VerticalNavLink
-      :item="{
-        title: 'Cards',
-        to: 'card-basic',
-        icon: { icon: 'mdi-credit-card-outline' }
-      }"
-    />
-    <VerticalNavLink
-      :item="{
-        title: 'Tables',
-        to: 'tables',
-        icon: { icon: 'mdi-table' }
-      }"
-    />
-    <VerticalNavLink
-      :item="{
-        title: 'Form Layouts',
-        to: 'form-layouts',
-        icon: { icon: 'mdi-form-select' }
-      }"
-    />
+    <VerticalNavLink :item="{
+      title: 'Typography',
+      to: 'typography',
+      icon: { icon: 'mdi-alpha-t-box-outline' }
+    }" />
+    <VerticalNavLink :item="{
+      title: 'Icons',
+      to: 'icons',
+      icon: { icon: 'mdi-eye-outline' }
+    }" />
+    <VerticalNavLink :item="{
+      title: 'Cards',
+      to: 'card-basic',
+      icon: { icon: 'mdi-credit-card-outline' }
+    }" />
+    <VerticalNavLink :item="{
+      title: 'Tables',
+      to: 'tables',
+      icon: { icon: 'mdi-table' }
+    }" />
+    <VerticalNavLink :item="{
+      title: 'Form Layouts',
+      to: 'form-layouts',
+      icon: { icon: 'mdi-form-select' }
+    }" />
   </ul>
 </template>
