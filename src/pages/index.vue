b3fc5eb2e053c168a43a98eaa76f46f9dc4c3eb8
Gabriel Vieira <gv1999mpc@hotmail.com>
2022-12-19 16:43:36 +0000

    fix: index merge error

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -27,7 +27,7 @@ onBeforeMount(async () => {
       <div class="float-md-end">
         <h1 class="text-h1 text-primary font-weight-bold text-uppercase">FasTuga</h1>
         <h4 class="text-h4 mt-auto mb-5">Very Fast. Very Tuga.</h4>
-        <router-link :to="{ path: '/menu' }" v-if="userStore.isAnonymous || userStore.isCustomer">
+        <router-link :to="{ name: 'menu' }" v-if="userStore.isAnonymous || userStore.isCustomer">
           <VBtn color="primary" depressed elevation="3" size="large">
             Order now</VBtn>
         </router-link>
@@ -44,18 +44,16 @@ onBeforeMount(async () => {
   <VRow>
     <VRow class="align-cards py-8 px-6 w-100 gap-8">
       <VCol v-for="product in products.slice(0, 6)" :key="product.id" cols="12" lg="3" sm="3">
-        <<<<<<< HEAD <VCard>
-          <VImg cover :aspect-ratio="1.5" :src="imageUrl(product.photo_url)" :lazy-src="imageUrl(product.photo_url)" />
-          =======
-          <VCard class="h-100">
-            <VImg cover :aspect-ratio="1" :src="imageUrl(product.photo_url)" :lazy-src="imageUrl(product.photo_url)" />
-            >>>>>>> origin/Landing-Page
-            <VCardItem>
-              <VCardTitle>
-                {{ product.name }}
-              </VCardTitle>
-            </VCardItem>
-          </VCard>
+        <!-- <VCard>
+            <VImg cover :aspect-ratio="1.5" :src="imageUrl(product.photo_url)" :lazy-src="imageUrl(product.photo_url)" /> -->
+        <VCard class="h-100">
+          <VImg cover :aspect-ratio="1" :src="imageUrl(product.photo_url)" :lazy-src="imageUrl(product.photo_url)" />
+          <VCardItem>
+            <VCardTitle>
+              {{ product.name }}
+            </VCardTitle>
+          </VCardItem>
+        </VCard>
       </VCol>
     </VRow>
   </VRow>
